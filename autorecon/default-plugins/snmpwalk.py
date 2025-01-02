0f618d2e8e735b000e1ba80067e862cda9455597
Zumi Yumi <150757931+ZumiYumi@users.noreply.github.com>
2025-01-02 12:59:57 -0800

    Update snmpwalk.py
    Removed redundant code. Added a NET-SNMP-EXTEND-MIB function. Troubleshooting why program is not executing it, command works fine on my machine but not in script.

--- autorecon/default-plugins/snmpwalk.py
+++ autorecon/default-plugins/snmpwalk.py
@@ -17,7 +17,7 @@ class SNMPWalk(ServiceScan):
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.1.6.0 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_system_processes.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.4.2.1.2 2>&1', outfile='{scandir}/{protocol}_{port}_snmp_snmpwalk_running_processes.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.4.2.1.4 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_process_paths.txt')
-		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.2.3.1.4 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_storage_units.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.2.3.1.4 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_software_names.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.4.1.77.1.2.25 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_user_accounts.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.6.13.1.3 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_tcp_ports.txt')
+		await service.execute('snmpwalk -c public -v 1 {address} NET-SNMP-EXTEND-MIB::nsExtendObjects 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_extend_objects.txt')
